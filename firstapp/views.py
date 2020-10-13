53abeeb91f7410d56d07fc16cf4792a060b24501
arkadie <preboe25@gmail.com>
2020-10-13 20:46:16 +0700

    game in Django

---
+++ firstapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'firstapp/index.html')
+
+
+def about(request):
+    return render(request, 'firstapp/about.html')
